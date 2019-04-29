5efa76fee5d68adafe6d96a4d01bb4e4e6d882fe
Alex <alexandruturcanu915@gmail.com>
2019-04-29 12:10:26 +0300

    Tested a concept.

--- src/main/java/com/shadowprince345/thefirearmy/blocks/tiles/TileDev.java
+++ src/main/java/com/shadowprince345/thefirearmy/blocks/tiles/TileDev.java
@@ -1,31 +1,12 @@
 package com.shadowprince345.thefirearmy.blocks.tiles;
 
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.items.ItemStackHandler;
 
-import javax.annotation.Nullable;
-
-public class TileDev extends TileEntity implements ITickable, ICapabilityProvider {
-
-    ItemStackHandler craftingMatrix = new ItemStackHandler(9);
+public class TileDev extends TileEntity implements ITickable{
 
     @Override
     public void update() {
 
-    }
-
-    @Override
-    public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing) {
-        return true;
-    }
-
-    @Nullable
-    @Override
-    public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing) {
-        return (T) craftingMatrix;
     }
 }
