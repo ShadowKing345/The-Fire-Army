5a10bf2a6bfba5d68919b2ef9f1cc456ffc94f2b
Alex <alexandruturcanu915@gmail.com>
2019-05-01 10:22:19 +0300

    made the entity item rotate.

--- src/main/java/com/shadowprince345/thefirearmy/client/tesr/RendererFireBlackSmithFurnace.java
+++ src/main/java/com/shadowprince345/thefirearmy/client/tesr/RendererFireBlackSmithFurnace.java
@@ -11,10 +11,14 @@ import net.minecraftforge.items.ItemHandlerHelper;
 
 public class RendererFireBlackSmithFurnace extends FastTESR<TileEntityFireBlacksmithFurnace> {
 
+    private static float partialTicks = 0;
+
     @Override
     public void renderTileEntityFast(TileEntityFireBlacksmithFurnace te, double x, double y, double z, float partialTicks, int destroyStage, float partial, BufferBuilder buffer) {
         EntityItem itemEntity = new EntityItem(Minecraft.getMinecraft().world, 0,0,0, ItemStack.EMPTY);
         itemEntity.hoverStart = 0;
+
+        this.partialTicks += partialTicks;
 
         for (int i = 0; i < 3; i++) {
             for (int k = 0; k < 3; k++) {
@@ -25,9 +29,9 @@ public class RendererFireBlackSmithFurnace extends FastTESR<TileEntityFireBlacks
                 itemEntity.setItem(itemStack);
                 GlStateManager.pushMatrix();
                 {
-                    GlStateManager.translate(x + 0.3125f + (0.1875 * i), y + 0.51f, z + 0.3125f + (0.1875 * k));
+                    GlStateManager.translate(x + 0.3125f + (0.1875 * i), y + 0.58f, z + 0.3125f + (0.1875 * k));
                     GlStateManager.scale(0.45, 0.45, 0.45);
-                    Minecraft.getMinecraft().getRenderManager().renderEntity(itemEntity, 0, 0, 0, 0f, 0, false);
+                    Minecraft.getMinecraft().getRenderManager().renderEntity(itemEntity, 0, 0, 0, 0f, this.partialTicks, false);
                 }
                 GlStateManager.popMatrix();
             }
