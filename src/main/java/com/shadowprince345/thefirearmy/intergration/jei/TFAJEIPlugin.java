c1dd2b873f0879af239b19005b2e5fa473853ad1
Alex <alexandruturcanu915@gmail.com>
2019-05-01 10:41:49 +0300

    added the recipe click areas for FBB and FBF.

--- src/main/java/com/shadowprince345/thefirearmy/intergration/jei/TFAJEIPlugin.java
+++ src/main/java/com/shadowprince345/thefirearmy/intergration/jei/TFAJEIPlugin.java
@@ -1,6 +1,8 @@
 package com.shadowprince345.thefirearmy.intergration.jei;
 
 import com.shadowprince345.thefirearmy.api.recipe.IFBBRecipe;
+import com.shadowprince345.thefirearmy.client.gui.GuiFBB;
+import com.shadowprince345.thefirearmy.client.gui.GuiFBF;
 import com.shadowprince345.thefirearmy.init.Blocks;
 import com.shadowprince345.thefirearmy.lib.FBBRecipesManager;
 import mezz.jei.api.*;
@@ -23,6 +25,9 @@ public class TFAJEIPlugin implements IModPlugin {
         jeiHelpers = registry.getJeiHelpers();
 
         registry.handleRecipes(IFBBRecipe.class, FBBRecipeWrapper::new, RecipeCategoriesUid.FBB);
+
+        registry.addRecipeClickArea(GuiFBB.class, 99, 33, 15, 13, RecipeCategoriesUid.FBB);
+        registry.addRecipeClickArea(GuiFBF.class, 85, 32, 15, 13, VanillaRecipeCategoryUid.SMELTING);
 
         registry.addRecipes(FBBRecipesManager.instance.getRecipes(), RecipeCategoriesUid.FBB);
         registry.addRecipeCategoryCraftingItem(new ItemStack(Blocks.blockFireBlacksmithFurnace), RecipeCategoriesUid.FBB);
