3bdd2f3c71ea37d701baeda40354c9a38875005a
Alex <alexandruturcanu915@gmail.com>
2019-05-01 21:27:54 +0300

    Added environmental features.

--- src/main/java/com/shadowprince345/thefirearmy/blocks/BlockFireFlower.java
+++ src/main/java/com/shadowprince345/thefirearmy/blocks/BlockFireFlower.java
@@ -4,10 +4,13 @@ import com.shadowprince345.thefirearmy.init.Blocks;
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.common.IShearable;
 
 import javax.annotation.Nonnull;
@@ -19,7 +22,6 @@ public class BlockFireFlower extends BlockBush implements IShearable{
 
     public BlockFireFlower() {
         setSoundType(SoundType.GLASS);
-
     }
 
     @Override
@@ -27,6 +29,16 @@ public class BlockFireFlower extends BlockBush implements IShearable{
         return net.minecraft.init.Items.BLAZE_POWDER;
     }
 
+    @Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        entityIn.setFire(5);
+    }
+
+    @Override
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        if(rand.nextBoolean())
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + rand.nextFloat(), pos.getY()  + rand.nextFloat(), pos.getZ()  + rand.nextFloat(), 0,rand.nextFloat()/20,0);
+    }
 
     @Override
     public boolean isShearable(@Nonnull ItemStack item, IBlockAccess world, BlockPos pos) {
--- src/main/java/com/shadowprince345/thefirearmy/blocks/BlockFireFlowerCrop.java
+++ src/main/java/com/shadowprince345/thefirearmy/blocks/BlockFireFlowerCrop.java
@@ -4,7 +4,14 @@ import com.shadowprince345.thefirearmy.init.Blocks;
 import com.shadowprince345.thefirearmy.init.Items;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlockFireFlowerCrop extends BlockCrops {
 
@@ -12,6 +19,18 @@ public class BlockFireFlowerCrop extends BlockCrops {
         setUnlocalizedName("fire_flower_crop");
         setRegistryName("fire_flower_crop");
         setSoundType(SoundType.GLASS);
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        if(state.getValue(this.AGE) == 7)
+            entityIn.setFire(5);
+    }
+
+    @Override
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        if(rand.nextBoolean() && stateIn.getValue(this.AGE) == 7)
+            worldIn.spawnParticle(EnumParticleTypes.FLAME, pos.getX() + rand.nextFloat(), pos.getY()  + rand.nextFloat(), pos.getZ()  + rand.nextFloat(), 0,rand.nextFloat()/20,0);
     }
 
     @Override
